c790c1dd15116b508aa2d0b16bc24f6ba4e81cb9
Omzig7 <43423565+Omzig7@users.noreply.github.com>
2022-01-15 17:29:28 -0500

    Create full-archive-search.py

---
+++ full-archive-search.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = "AAAAAAAAAAAAAAAAAAAAABmiUgEAAAAA6h2vLrTsUWElFlXE1cutt7stObk%3DDUaoRcksG7VjubGkvCYk01BT16sV0IhmneaXqG0VaO1wOhgAIq"
+
+search_url = "https://api.twitter.com/2/tweets/search/all"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+# https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+
+# See here for example queries: https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+# Full archine search: https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/full-archive-search
+#query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
+#query_params = {'query': '(happy OR happiness) place_country:US -birthday -is:retweet'}
+query_params = {'query': 'test place_country:US -birthday -is:retweet'}
+
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FullArchiveSearchPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.request("GET", search_url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
