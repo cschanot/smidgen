44c61b6570a0ca13f622f43351cce62e9cde9eba
Chris Schanot <cschanot@gmail.com>
2022-01-15 20:11:52 -0500

    Added flask with sub directories

---
+++ smflask.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, jsonify
+import configparser
+import requests
+import json
+
+config = configparser.ConfigParser()
+config.read("../../twitter/config.ini")
+
+
+
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = config['API_Settings']['bearer_token']
+print(bearer_token)
+search_url = "https://api.twitter.com/2/tweets/search/all"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+# https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+
+# See here for example queries: https://developer.twitter.com/en/docs/twitter-api/tweets/search/integrate/build-a-query
+# Full archine search: https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/full-archive-search
+#query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
+#query_params = {'query': '(happy OR happiness) place_country:US -birthday -is:retweet'}
+query_params = {'query': 'test place_country:US -birthday -is:retweet'}
+
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FullArchiveSearchPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.request("GET", search_url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+json_response = connect_to_endpoint(search_url, query_params)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',json=json_response)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=6969,debug=True)
