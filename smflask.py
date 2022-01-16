edb1dd71ab47a14f0e5eabded8bd42f4042431b3
Chris Schanot <cschanot@gmail.com>
2022-01-15 21:57:37 -0500

    smflask changed

--- smflask.py
+++ smflask.py
@@ -3,15 +3,10 @@ import configparser
 import requests
 import json
 
-config = configparser.ConfigParser()
-config.read("../../twitter/config.ini")
-
-
-
 # To set your environment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
-bearer_token = config['API_Settings']['bearer_token']
-print(bearer_token)
+bearer_token = "AAAAAAAAAAAAAAAAAAAAABmiUgEAAAAA6h2vLrTsUWElFlXE1cutt7stObk%3DDUaoRcksG7VjubGkvCYk01BT16sV0IhmneaXqG0VaO1wOhgAIq"
+
 search_url = "https://api.twitter.com/2/tweets/search/all"
 
 # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
@@ -42,13 +37,30 @@ def connect_to_endpoint(url, params):
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
     return response.json()
-json_response = connect_to_endpoint(search_url, query_params)
 
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2FullArchiveSearchPython"
+    return r
+
+
+def connect_to_endpoint(url, params):
+    response = requests.request("GET", search_url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    json_response = connect_to_endpoint(search_url, query_params)
     return render_template('index.html',json=json_response)
 
 
