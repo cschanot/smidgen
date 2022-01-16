1d8f08357380358123700adcf57344591dad3cfd
Chris Schanot <cschanot@gmail.com>
2022-01-15 23:16:25 -0500

    commit it

--- user_context.py
+++ user_context.py
@@ -1,12 +1,25 @@
+
+import configparser
+import requests
+import json
+
 from requests_oauthlib import OAuth1Session
 import json
+import configparser
+
+config = configparser.ConfigParser()
+config.read("c:/twitter/config.ini")
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'CONSUMER_KEY'='<your_consumer_key>'
 # export 'CONSUMER_SECRET'='<your_consumer_secret>'
 
-consumer_key = "gYjLgRU2gyjuBYs5u4hKTXk26"
-consumer_secret = "5ivuMrlBAdU3gEtGcyp5IthwnSNkxCAE1M9Pt4BRrQfkP77fcW"
+consumer_key = config['API_Settings']['consumer_key']
+consumer_secret =  config['API_Settings']['consumer_secret']
+access_token = config['API_Settings']['access_token']
+access_token_secret = config['API_Settings']['access_token_secret']
+
+
 
 # You can adjust ids to include a single Tweets
 # Or you can add to up to 100 comma-separated IDs
@@ -31,28 +44,13 @@ except ValueError:
 
 resource_owner_key = fetch_response.get("oauth_token")
 resource_owner_secret = fetch_response.get("oauth_token_secret")
-print("Got OAuth token: %s" % resource_owner_key)
-
-# Get authorization
-base_authorization_url = "https://api.twitter.com/oauth/authorize"
-authorization_url = oauth.authorization_url(base_authorization_url)
-print("Please go here and authorize: %s" % authorization_url)
-verifier = input("Paste the PIN here: ")
-
-# Get the access token
-access_token_url = "https://api.twitter.com/oauth/access_token"
-oauth = OAuth1Session(
-    consumer_key,
-    client_secret=consumer_secret,
-    resource_owner_key=resource_owner_key,
-    resource_owner_secret=resource_owner_secret,
-    verifier=verifier,
-)
-oauth_tokens = oauth.fetch_access_token(access_token_url)
 
 
-access_token = oauth_tokens["oauth_token"]
-access_token_secret = oauth_tokens["oauth_token_secret"]
+# oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+
+# access_token = oauth_tokens["oauth_token"]
+# access_token_secret = oauth_tokens["oauth_token_secret"]
 
 # Make the request
 oauth = OAuth1Session(
@@ -63,7 +61,7 @@ oauth = OAuth1Session(
 )
 
 response = oauth.get(
-    "https://api.twitter.com/2/tweets", params=params
+    "https://api.twitter.com/2/tweets/search/recent", params=params
 )
 
 if response.status_code != 200:
