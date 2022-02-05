8dffae617d544926587694ee2d4f5b3ab6eacccb
Chris Schanot <cschanot@gmail.com>
2022-02-05 16:52:07 -0500

    keep typing words until it works!

--- static/app.js
+++ static/app.js
@@ -15,7 +15,7 @@ $(window).on("scroll", function () {
 })
 function Yeet_Word(tweet_word) {
   alert(tweet_word);
-  previous_query = document.getElementById("tweet").innerHTML;
+  previous_query = document.getElementById("tweet").innerHTML.value;
   new_query = previous_query + ' ' + tweet_word;
-  document.getElementId("tweet").innerHTML(new_query);
+  document.getElementId("tweet").innerHTML.value = new_query;
   }
