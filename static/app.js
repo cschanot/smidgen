500354b81fcb589d08f2eb39849fe3752dafefde
Matt Chandler <Omzig7@gmail.com>
2022-02-19 18:05:04 -0500

    ?

--- static/app.js
+++ static/app.js
@@ -31,8 +31,8 @@ var count = 0;
 function extra_query()
 {
   count++
-  var tweet_label = '<label for="tweet">Tweet (' += count += ')</label>'
-  var tweet_input = '<input type="text" name="result_array[]" value="" /><br>'
+  var tweet_label = '<label for="tweet">Tweet (' += count += ')</label>';
+  var tweet_input = '<input type="text" name="result_array[]" value="" /><br>';
 
   document.getElementById('tweet_in').append(tweet_label).hide().fadeIn('1000');
   document.getElementById('tweet_in').append(tweet_input).hide().fadeIn('1000');
