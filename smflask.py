448d32f0a097149deb4ed6ab356c97ae7cb477bd
Matt Chandler <Omzig7@gmail.com>
2022-03-01 13:48:57 -0500

    multi: 👌, single: 😡

--- smflask.py
+++ smflask.py
@@ -175,7 +175,7 @@ def index():
 
         # If single query is not empty and multi-query was not ran.
         print(tweet)
-        if (tweet):
+        if (len(tweet) > 0):
             query_params = {'query': '%s place_country:US -birthday -is:retweet' % tweet,
                             'tweet.fields': 'public_metrics,created_at,lang,source',
                             'expansions': 'author_id',
