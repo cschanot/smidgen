3eaeea64a0ff681b5ba7c9c6ebe9bc94fb0d5d4b
gizmo-udel <90262038+gizmo-udel@users.noreply.github.com>
2022-03-19 21:03:03 -0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-const port = 3030;
-
-app.use(express.static(__dirname + '/static'));
-
-app.listen(port, function() {
-    console.log('Express server listening on port ' + port);
-});
