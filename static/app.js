e370b56a83b70ea2f7dfaef28502310cd41638db
gizmo-udel <90262038+gizmo-udel@users.noreply.github.com>
2022-03-19 21:04:16 -0400

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,62 @@
+// Navbanner show ul on click
+$(document).ready(function () {
+  $(".menu-icon").on("click", function () {
+    $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling effect for navbar
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
+})
+
+function Yeet_Word(tweet_word) {
+  console.log(tweet_word)
+  //alert(tweet_word);
+  previous_query = document.getElementById("tweet").value;
+  new_query = previous_query + ' ' + tweet_word;
+  console.log(new_query)
+  document.getElementById("tweet").value = new_query;
+}
+
+function tweet_input() {
+
+}
+var count = 0;
+/*
+var tweet_input = document.createElement("form");
+tweet_input.setAttribute("class", "tweet_input");
+tweet_input.setAttribute("id", "tweet_input");
+tweet_input.setAttribute("value", "tweet_input");
+*/
+
+/*
+function extra_query()
+{
+  count++
+  var tweet_label = '<label for="tweet">Tweet (' += count += ')</label>';
+  var tweet_input = '<input type="text" name="result_array[]" value="" /><br>';
+
+  document.getElementById('tweet_in').insertAdjacentHTML("beforeend", tweet_label);
+  document.getElementById('tweet_in').insertAdjacentHTML("beforeend", tweet_input);
+  count++;
+}
+*/
+
+// Dynamically add extra query spaces for a multi-query.
+document.getElementById("add_tweet_query_button").onclick = function() {
+  var label = document.createElement("div");
+  var input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("name", "result_array[]");
+  input.setAttribute("value", "");
+  input.setAttribute("id", "multi_tweet");
+  label.setAttribute("value", "Tweet:")
+  label.appendChild(input);
+  document.getElementById("multi_query").appendChild(label);
+  return false;
+};
